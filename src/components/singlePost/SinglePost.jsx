3af07b5decb31f268b532bdaa2db5e5f1d581d2c
Pankaj Dharmik <pankajdharmik4@gmail.com>
2022-09-13 00:53:00 +0530

    Single Post Added

---
+++ src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,23 @@
+import './singlepost.css'
+
+export default function SinglePost() {
+  return (
+    <div className='singlePost'>
+        <div className="singlePostWrapper">
+            <img src="https://bobraley.files.wordpress.com/2016/08/image1.jpeg?w=1800" alt="" className="singlePostImg" />
+            <h1 className="singlePostTitle">
+                Lorem ipsum dolor sit amet.
+                <div className="singlePostEdit">
+                    <i class="singlePostIcon fa-regular fa-pen-to-square"></i>
+                    <i class="singlePostIcon fa-solid fa-trash"></i>
+                </div>
+            </h1>
+            <div className="singlePostInfo">
+                <span className='singlePostAuthor'>Author :<b>Safak</b></span>
+                <span className='singlePostDate'>1 hour ago</span>
+            </div>
+            <p className='singlePostDesc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque officiis quidem vero placeat accusamus sapiente obcaecati animi eaque ullam pariatur laborum commodi possimus accusantium consequuntur, nesciunt magnam ea, molestias optio alias quisquam? Eligendi itaque blanditiis in laboriosam nesciunt. Libero reiciendis velit voluptatum repudiandae mollitia dolore perferendis ad at deserunt quas dignissimos possimus, nihil suscipit distinctio dolorem ea officia molestiae minima magni commodi? Totam itaque voluptates numquam nobis similique laboriosam dolore qui cupiditate provident culpa unde autem excepturi adipisci at temporibus assumenda aperiam, nihil aliquam debitis cum dolorem! Ducimus quis quia dolorum quasi rerum sed totam ex odit eius, doloribus corrupti repudiandae possimus numquam saepe non tenetur vitae quaerat pariatur veniam id impedit facere consequatur sunt quos? A, provident. Cum est sit earum autem fugit amet nisi itaque doloremque beatae, veritatis aliquam tenetur similique ex! Aut modi incidunt ducimus dolor quos repudiandae consequuntur perspiciatis dolorum, veniam mollitia ab voluptas odit fuga laudantium quasi nam voluptatem nostrum consectetur suscipit molestiae labore reiciendis. Facilis molestiae cupiditate, modi aliquam recusandae deleniti autem! Impedit nobis fugiat voluptate eum eius tempora illum labore enim dolores! Earum et laboriosam exercitationem atque repellendus non perferendis laborum repudiandae veniam ab dolores sit ea facilis eos alias veritatis iusto recusandae fugiat incidunt at dolorum, quos cum dignissimos. Quis ipsa esse laudantium. Temporibus minus, dolor voluptates dolores cupiditate reiciendis obcaecati illum pariatur numquam sapiente iure, voluptatibus commodi eaque? Consequuntur accusamus fugit deserunt nisi quasi illum sit veniam, nihil, exercitationem sint quibusdam. Velit fugiat soluta incidunt unde voluptatem cum est amet, laboriosam magnam, earum quaerat, aperiam culpa deleniti minima tenetur vero. In, ad suscipit quia enim ex, laborum dignissimos harum reprehenderit ea, deserunt voluptas dolore corrupti necessitatibus. At delectus dicta neque veritatis quos reiciendis, nesciunt ducimus cupiditate sapiente iure minus quia tenetur corporis assumenda laudantium, enim velit consequuntur reprehenderit hic. Inventore, aspernatur.</p>
+        </div>
+    </div>
+  )
+}
