64ed56e490b11bc579ae5cd5ded3361a9a913497
Pankaj Dharmik <pankajdharmik4@gmail.com>
2022-09-13 10:43:17 +0530

    Login Register page added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Single from "./pages/single/Single";
 import TopBar from "./components/topbar/TopBar";
 import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+
 
 function App() {
   return (
     <>
       <TopBar/>
-      <Settings/>
+      <Register/>
     </>
     );
 }
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,19 @@
+import "./register.css"
+
+export default function Login() {
+  return (
+    <div className="register">
+        <span className="registerTitle">Register</span>
+        <form className="registerForm" >
+          <label>Username</label>
+            <input className="registerinput" type="text" placeholder="Enter your username..."/>
+            <label>Email</label>
+            <input className="registerinput" type="text" placeholder="Enter your email..."/>
+            <label>Password</label>
+            <input className="registerinput" type="password" placeholder="Enter your password..."/>
+            <button className="registerButton">Register</button>
+        </form>
+        <button className="registerLoginButton">Login</button>
+    </div>
+  )
+}
